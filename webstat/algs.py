bfac343838302c20c5577a001b4d006f5d2745af
daz9e <grigpavel21@ya.ru>
2023-05-10 13:38:24 +0300

    little changes

--- webstat/algs.py
+++ webstat/algs.py
@@ -8,13 +8,17 @@ def addusers(filename):
     with open(path_to_file, 'r', encoding='utf-8') as f:
         data = json.load(f)
         messages = data["messages"]
+        print("handle messages!")
     for message in messages:
         try:
-            id = message["from_id"][4:]
-            name = message["from"]
-            user = User(user_id=id, username=name)
-            user.save()
-        except:
+            id = message.get("from_id", "")[4:]
+            name = message.get("from", "")
+            user, created = User.objects.get_or_create(user_id=id, username=name)
+            if created:
+                print(f"New user created: {name} ({id})")
+        except Exception as e:
+            print(f"Error creating user: {e}")
             continue
+
 
 
